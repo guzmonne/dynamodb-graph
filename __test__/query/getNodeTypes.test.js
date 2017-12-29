7a9bf42abf50e5eed6d6b2304ae06e04572a3c20
Guzman Monne <guzmonne@hotmail.com>
2017-12-29 18:10:56 -0300

    created a getNodeTypes function

---
+++ __test__/query/getNodeTypes.test.js
@@ -0,0 +1,172 @@
+'use strict';
+
+var sinon = require('sinon');
+var cuid = require('cuid');
+var chunk = require('lodash/chunk.js');
+var range = require('lodash/range.js');
+var getNodeTypesFactory = require('../../src/query/getNodeTypes.js');
+var utils = require('../../src/modules/utils.js');
+
+describe('getNodeTypesFactory()', () => {
+  var node = cuid();
+  var types = [cuid(), cuid(), cuid()];
+  var maxGSIK = 10;
+  var table = 'TestTable';
+  var documentClient = {
+    batchGet: params => ({
+      promise: () => {
+        var attribute = Math.random() * 10 > 5 ? 'String' : 'Number';
+        var data = type => (attribute === 'String' ? type : type.length);
+        return Promise.resolve({
+          Items: params.RequestItems[table].Keys.map(key =>
+            Object.assign({}, key, {
+              [attribute]: data(key.Type)
+            })
+          )
+        });
+      }
+    })
+  };
+  var config = { table, maxGSIK, documentClient };
+
+  test('should be a function', () => {
+    expect(typeof getNodeTypesFactory).toEqual('function');
+  });
+
+  test('should call the `utils.checkConfiguration` function', () => {
+    sinon.spy(utils, 'checkConfiguration');
+    getNodeTypesFactory(config);
+    expect(utils.checkConfiguration.calledOnce).toBe(true);
+    utils.checkConfiguration.restore();
+  });
+
+  describe('#getNodeTypes()', () => {
+    var getNodeTypes = getNodeTypesFactory(config);
+
+    test('should throw if `node` is undefined', () => {
+      expect(() => getNodeTypes()).toThrow('Node is undefined');
+    });
+
+    test('should throw if `types` is not a list', () => {
+      expect(() => getNodeTypes({ node })).toThrow('Types is not a list');
+    });
+
+    test('should return a promise', () => {
+      expect(getNodeTypes({ node, types }) instanceof Promise).toBe(true);
+    });
+
+    beforeEach(() => {
+      sinon.spy(documentClient, 'batchGet');
+    });
+
+    afterEach(() => {
+      documentClient.batchGet.restore();
+    });
+
+    test('should call the `documentClient.batchGet` function', () => {
+      return getNodeTypes({ node, types }).then(() => {
+        expect(documentClient.batchGet.calledOnce).toBe(true);
+      });
+    });
+
+    test('should call the `documentClient.batchGet` function with valid params', () => {
+      return getNodeTypes({ node, types }).then(() => {
+        expect(documentClient.batchGet.args[0][0]).toEqual({
+          RequestItems: {
+            [table]: {
+              Keys: types.map(type => ({
+                Node: node,
+                Type: type
+              }))
+            }
+          }
+        });
+      });
+    });
+
+    test('should split the types reques in chunks of 100', () => {
+      var types = range(0, 1000).map(cuid);
+
+      return getNodeTypes({ node, types }).then(() => {
+        expect(documentClient.batchGet.callCount).toEqual(10);
+        expect(documentClient.batchGet.args[0][0]).toEqual({
+          RequestItems: {
+            [table]: {
+              Keys: range(0, 100).map(i => ({
+                Node: node,
+                Type: types[i]
+              }))
+            }
+          }
+        });
+        expect(documentClient.batchGet.args[9][0]).toEqual({
+          RequestItems: {
+            [table]: {
+              Keys: range(900, 1000).map(i => ({
+                Node: node,
+                Type: types[i]
+              }))
+            }
+          }
+        });
+      });
+    });
+
+    test('should combine all the items returned', () => {
+      var types = range(0, 1000).map(cuid);
+
+      return getNodeTypes({ node, types }).then(result => {
+        expect(result.Items.length).toEqual(1000);
+      });
+    });
+
+    test('should parse the Items returned', () => {
+      var types = range(0, 1000).map(cuid);
+
+      return getNodeTypes({ node, types }).then(result => {
+        range(0, 1000).forEach(i => {
+          expect(!!result.Items[i].Data).toBe(true);
+        });
+      });
+    });
+
+    test('should continue trying to read from the table if `UnprocessedKeys` is returned', () => {
+      var types = range(0, Math.floor(1000 + Math.random() * 1000) * 2).map(
+        cuid
+      );
+      documentClient.batchGet.restore();
+      sinon.stub(documentClient, 'batchGet').callsFake(params => ({
+        promise: () => {
+          var attribute = Math.random() * 10 > 5 ? 'String' : 'Number';
+          var data = type => (attribute === 'String' ? type : type.length);
+          var result;
+          var keys = params.RequestItems[table].Keys;
+          if (Math.round(Math.random()) < 0.999) {
+            result = {
+              Items: keys.slice(0, keys.length / 2).map(key =>
+                Object.assign({}, key, {
+                  [attribute]: data(key.Type)
+                })
+              ),
+              UnprocessedKeys: keys.slice(keys.length / 2, keys.length)
+            };
+          } else {
+            result = {
+              Items: keys.map(key =>
+                Object.assign({}, key, {
+                  [attribute]: data(key.Type)
+                })
+              )
+            };
+          }
+          return Promise.resolve(result);
+        }
+      }));
+
+      return getNodeTypes({ node, types }).then(result => {
+        expect(result.Items.length).toBe(types.length);
+        expect(documentClient.batchGet.callCount >= 10).toBe(true);
+      });
+    });
+  });
+});
---
+++ src/query/getNodeTypes.js
@@ -0,0 +1,106 @@
+'use stict';
+
+var chunk = require('lodash/chunk.js');
+var get = require('lodash/get.js');
+var utils = require('../modules/utils.js');
+
+/**
+ * Factory that returns a function that attempts to get a bunch of Node items
+ * by type.
+ * @param {function} item - Item creator function.
+ * @param {ConfigObject} config - Main configuration object.
+ * @return {function} Function that attempts to create a new node.
+ */
+module.exports = function getNodeTypesFactory(config) {
+  var { tenant = '', documentClient, table } = config;
+
+  function batchGetNodePromise(node, types, previousResponse) {
+    var params = {
+      RequestItems: {
+        [table]: {
+          Keys: types.map(type => ({
+            Node: node,
+            Type: type
+          }))
+        }
+      }
+    };
+
+    if (process.env.DEBUG) {
+      params.ReturnConsumedCapacity = 'INDEXES';
+      params.ReturnItemCollectionMetrics = 'SIZE';
+    }
+
+    return new Promise((resolve, reject) => {
+      return documentClient
+        .batchGet(params)
+        .promise()
+        .then(response => {
+          if (typeof previousResponse === 'object') {
+            var accumulatedResponses = {
+              Count:
+                get(previousResponse, 'Count', 0) + get(response, 'Count', 0),
+              ScannedCount:
+                get(previousResponse, 'ScannedCount', 0) +
+                get(response, 'ScannedCount', 0),
+              Items: get(previousResponse, 'Items', []).concat(
+                get(response, 'Items', [])
+              )
+            };
+
+            if (response.UnprocessedKeys)
+              accumulatedResponses.UnprocessedKeys = response.UnprocessedKeys;
+
+            return accumulatedResponses;
+          }
+          return response;
+        })
+        .then(response => {
+          if (Array.isArray(response.UnprocessedKeys) === true)
+            return batchGetNodePromise(
+              node,
+              response.UnprocessedKeys,
+              response
+            );
+          return response;
+        })
+        .then(utils.parseResponse)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  utils.checkConfiguration(config);
+
+  /**
+   * Functions that attempts to get various types of items from the table.
+   * @return {Promise} BatchGet DynamoDB request promise.
+   */
+  return function getNodeTypes(options = {}) {
+    var { node, types } = options;
+
+    if (node === undefined) throw new Error('Node is undefined');
+    if (Array.isArray(types) === false) throw new Error('Types is not a list');
+
+    return Promise.all(
+      chunk(types, 100).map(chunk => {
+        var results = [];
+        return batchGetNodePromise(node, chunk);
+      })
+    ).then(results => {
+      return results.reduce(
+        (acc, result) => {
+          acc.Items = acc.Items.concat(result.Items);
+          acc.Count += result.Count || 0;
+          acc.ScannedCount += result.ScannedCount || 0;
+          return acc;
+        },
+        {
+          Count: 0,
+          ScannedCount: 0,
+          Items: []
+        }
+      );
+    });
+  };
+};
