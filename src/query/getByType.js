2c3701e6be5deefa1f455d1d976e9ed847651847
Guzman Monne <guzmonne@hotmail.com>
2017-12-29 11:56:10 -0300

    removed uneccessary variables

--- src/query/getByType.js
+++ src/query/getByType.js
@@ -1,12 +1,7 @@
 'use strict';
 
-var range = require('lodash/range');
-var utils = require('../modules/utils.js');
 var getByFactory = require('../general/getBy.js');
 
-var START_GSIK = 0;
-var DEFAULT_GSIK_LIST_SIZE = 10;
-var LIMIT = 10;
 /**
  * Factory that returns a function that attempts query the table indexed by
  * GSIK, through the `ByType` GSI, and sorted by type.
