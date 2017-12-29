78a1a16ceca9ea65132d6a146f98c0b46d79a154
Guzman Monne <guzmonne@hotmail.com>
2017-12-29 12:03:33 -0300

    created a getByData function

---
+++ src/query/getByData.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var getByFactory = require('../general/getBy.js');
+
+/**
+ * Factory that returns a function that attempts query the table indexed by
+ * GSIK, through the `ByType` GSI, and sorted by type.
+ * @param {ConfigObject} config - Main configuration object.
+ * @return {function} Function that attempts to create a new node.
+ */
+module.exports = function getByTypeFactory(config = {}) {
+  return getByFactory('Data', config);
+};
